850503ff7d167ca54b6842cbfb2a0a081ca1d789
magnocarv <magnocarv@hotmail.com>
2020-04-22 05:14:04 -0300

    cube in camera add

---
+++ Assets/script/CubeRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(Random.value, Random.value, Random.value));
+        this.transform.RotateAround(Vector3.zero, Vector3.up, 1.0f);
+    }
+}
